542ddfdfc31fe43f736fcaf4708e1444f88455de
WW930\A694392 <martyna.strzempek.external@atos.net>
2018-09-23 19:07:07 +0200

    add note in every result

--- components/setResultModal/setResultModal.vue
+++ components/setResultModal/setResultModal.vue
@@ -10,6 +10,10 @@
       </div>
     </div>
     <p class="validation-text" :class="{ hidden: isMarked }">To Add the goal You must fill in name of the goal and name of the subgoal</p>
+    <div class="textarea-wrapper" :class="{ hidden: isGoalDone }">
+      <p class="question">Why haven't You done that today?</p>
+      <textarea class="textarea" v-model="note" cols="77" rows="5"></textarea>
+    </div>
     <span slot="footer" class="dialog-footer">
       <el-button @click="cancel">Cancel</el-button>
       <el-button type="primary" @click="save">Save</el-button>
@@ -19,17 +23,23 @@
 
 <script>
 import * as ACTIONS from "../../store/actionTypes";
+import NoteTextarea from "../../components/NoteTextarea/NoteTextarea.vue";
 export default {
   data() {
     return {
       status: "",
       isMarked: true,
+      isGoalDone: true,
+      note: ""
     }
   },
   props: {
     visibility: Boolean,
     goalId: Number,
     resultId: String,
+  },
+  components: {
+    NoteTextarea,
   },
   methods: {
     cancel() {
@@ -46,10 +56,20 @@ export default {
           resultId: this.resultId,
           status: this.status
         });
+        this.$store.dispatch(ACTIONS.SET_NOTE, {
+          goalId: this.goalId,
+          resultId: this.resultId,
+          note: this.note,
+        })
       } else {
         this.isMarked = false;
       }
       this.status = "";
+    }
+  },
+  watch: {
+    status() {
+      this.isGoalDone = this.status !== "notDone";
     }
   }
 }
@@ -67,6 +87,13 @@ export default {
 .validation-text {
   padding: 20px 0 0 20px;
   color: red;
+}
+.textarea-wrapper {
+  margin-top: 20px;
+  margin-left: 20px;
+  .question {
+    color: red;
+  }
 }
 .hidden {
   display: none;
